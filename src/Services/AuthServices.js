078aa6d70bfab82dfb500cd026afe9669fc6cc8b
MoisesML <69059086+MoisesML@users.noreply.github.com>
2020-12-21 00:36:16 -0500

    deployment

--- src/Services/AuthServices.js
+++ src/Services/AuthServices.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
+// const URL = 'http://127.0.0.1:5000';
+const URL = 'https://projecto-codigo-final.herokuapp.com';
+
 const registrarPersona = async (objPersona) => {
     try {
+        const URI = `${URL}/registro`;
         let headers = {
             "Content-Type":"application/json",
         };
-        let datos = axios.post('http://127.0.0.1:5000/registro/', objPersona, { headers });
+        let datos = axios.post(URI, objPersona, { headers });
         return datos;
     } catch (error) {
         return error
@@ -14,10 +18,11 @@ const registrarPersona = async (objPersona) => {
 
 const loginPersona = async (objCredenciales) => {
     try {
+        const URI = `${URL}/login`;
         let headers = {
             "Content-Type":"application/json"
         };
-        let datos = axios.post('http://127.0.0.1:5000/login/', objCredenciales, { headers })
+        let datos = axios.post(URI, objCredenciales, { headers })
         return datos
     } catch (error) {
         return error;
--- src/Services/PersonaServices.js
+++ src/Services/PersonaServices.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const URL = 'http://127.0.0.1:5000'
+// const URL = 'http://127.0.0.1:5000';
+const URL = 'https://projecto-codigo-final.herokuapp.com';
 
 const informacionPersona = async (id) => {
     try {
